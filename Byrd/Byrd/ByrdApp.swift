c21b8070282ce3a76f15c83b9a240ef2dc221e83
Fernando de Lucas <54122934+FernandoDLucas@users.noreply.github.com>
2023-05-28 11:55:22 -0300

    setup project

---
+++ Byrd/Byrd/ByrdApp.swift
@@ -0,0 +1,17 @@
+//
+//  ByrdApp.swift
+//  Byrd
+//
+//  Created by Fernando de Lucas da Silva Gomes on 28/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ByrdApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
